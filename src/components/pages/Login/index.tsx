e887e43035613ccbee596bd7f6ee9ae6558417b3
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-09-18 23:44:14 -0300

    feat: adjust layout login

--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -1,15 +1,26 @@
 // External Libraries
 import React from "react";
+import { FaTruckMedical } from "react-icons/fa6";
 
 // Components
 import { Text } from "../../commons/toolkit/Text";
+import { Link } from "../../commons/toolkit/Link";
+import { Input } from "../../commons/toolkit/Input";
+import { Button } from "../../commons/toolkit/Button";
 
 // Hooks
 import { useLogin } from "./hooks/useLogin";
 
 // Styles
-import { CardLogin, Container, ImageContent } from "./styles";
-import { Input } from "../../commons/toolkit/Input";
+import {
+  CardLogin,
+  Container,
+  Form,
+  IconContainer,
+  ImageContent,
+  InputsContainer,
+  TextContainer,
+} from "./styles";
 
 export const Login: React.FC = () => {
   // Hooks
@@ -17,14 +28,32 @@ export const Login: React.FC = () => {
 
   return (
     <Container>
-      <ImageContent>
+      <ImageContent></ImageContent>
 
-      </ImageContent>
-      
       <CardLogin>
-        <Text fontSize="2rem">Bem vindo</Text>
-        <Input label="E-mail:" type="text"></Input>
-        <Input label="Senha:" type="password" />
+        <IconContainer>
+          <FaTruckMedical color="#84cae8" size='3rem' />
+        </IconContainer>
+
+       
+        <Form>
+          <InputsContainer>
+            <Input
+              label="E-mail:"
+              type="text"
+              name="email"
+              placeholder="Digite seu e-mail"
+            />
+            <Input label="Senha:" type="password" />
+          </InputsContainer>
+
+          <Button label="Acessar" size="100%" />
+        </Form>
+
+        <TextContainer>
+          <Text>Ainda não possui login?</Text>
+          <Link label={"Cadastre-se"} href={""}></Link>
+        </TextContainer>
       </CardLogin>
     </Container>
   );
