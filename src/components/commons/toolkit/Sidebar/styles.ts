2d7aa8b033343bdb58cec361f7685645f1d0e0c8
PolyStefane <polyvstefane@gmail.com>
2024-09-20 02:49:46 -0300

    feat: new exit button sidebar

--- src/components/commons/toolkit/Sidebar/styles.ts
+++ src/components/commons/toolkit/Sidebar/styles.ts
@@ -1,12 +1,27 @@
 import styled from "styled-components";
 
+interface Props {
+    $justifyContent?: string;
+}
+
 export const SidebarContainer = styled.div`
-  width: 250px;
+  width: 270px;
   background-color: #177198;
   padding: 20px;
   height: 100%;
   color: #F3FAFD;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TopMenu = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
 `;
 
 export const Logo = styled.div`
@@ -23,7 +38,7 @@ export const Menu = styled.ul`
   padding: 0;
 `;
 
-export const MenuItem = styled.li`
+export const MenuItem = styled.li<Props>`
   margin: 8px 0;
   cursor: pointer;
   font-size: 18px;
@@ -32,4 +47,7 @@ export const MenuItem = styled.li`
   width: 100%;
   height: 50px;
   gap: 10px;
+
+  justify-content: ${({$justifyContent}) => $justifyContent ? $justifyContent : "normal"};
 `;
+
