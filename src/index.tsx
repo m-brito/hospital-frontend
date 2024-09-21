3a60e66af288c6d0c0b4793beea1cd6b3fac19b2
m-brito <britomauricio2003@gmail.com>
2024-09-20 21:40:38 -0300

    feat: add doctor listing route

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { SignUp } from "./components/pages/SignUp";
 import { Home } from "./components/pages/Home";
 import { DoctorsList } from "./components/pages/DoctorsList";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Layout } from "./components/layout";
+import { Layout } from "./components/Layout/layout";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -21,6 +21,7 @@ root.render(
         <Route path="signup" element={<SignUp />} />
         <Route path="/home" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="doctors" element={<DoctorsList />} /> 
         </Route>
       </Routes>
     </BrowserRouter>
