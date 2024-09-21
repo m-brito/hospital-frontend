ac43fa2c40922a3a695d6b5a07f57a8e3ffe0a0a
m-brito <britomauricio2003@gmail.com>
2024-09-20 21:43:00 -0300

    fix: fixes layout

--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { Sidebar } from "./commons/toolkit/Sidebar";
 import { Outlet } from "react-router-dom";
-import { Container } from "./pages/Login/styles";
-import { Content } from "./pages/Home/styles";
+import { Sidebar } from "../commons/toolkit/Sidebar";
+import { Container, Main } from "./styles";
 
 export const Layout = () => {
   return (
     <Container>
       <Sidebar />
-      <Content>
+      <Main>
         <Outlet />
-      </Content>
+      </Main>
     </Container>
   );
 };
--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,19 +1,11 @@
-// External Libraries
-import React from "react";
-import { Sidebar } from "../../commons/toolkit/Sidebar";
-
 // Styles
-import { Container, Content, Title, Paragraph } from "./styles";
+import { Content, Title, Paragraph } from "./styles";
 
 export const Home = () => {
-
   return (
-    <Container>
-      <Sidebar />
-      <Content>
-        <Title>Bem-vindo ao Sistema Hospitalar</Title>
-        <Paragraph>Escolha uma opção no menu lateral para começar.</Paragraph>
-      </Content>
-    </Container>
+    <Content>
+      <Title>Bem-vindo ao Sistema Hospitalar</Title>
+      <Paragraph>Escolha uma opção no menu lateral para começar.</Paragraph>
+    </Content>
   );
 };
