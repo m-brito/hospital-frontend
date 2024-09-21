fac17bd008a3e84fa0a036ccfc7c554b3d33bd48
m-brito <britomauricio2003@gmail.com>
2024-09-21 01:11:37 -0300

    fix: checks if the user exists in the localStorage in the `useLogin` hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Login } from './components/pages/Login';
 import Home from './components/pages/Home';
 import SignUp from './components/pages/SignUp'; 
