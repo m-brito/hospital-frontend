ab1a1a79a0b66d243fe8111973f7ea4db24cf312
m-brito <britomauricio2003@gmail.com>
2024-09-23 06:12:21 -0300

    refactor: remove unnecessary imports

--- src/components/commons/toolkit/Sidebar/constants/menuItemsByRole.tsx
+++ src/components/commons/toolkit/Sidebar/constants/menuItemsByRole.tsx
@@ -10,10 +10,10 @@ import { MenuItemInfos } from "../MenuItem/types/MenuItemInfos";
 const menuItems: MenuItemInfos[] = [
   { icon: <IoHomeSharp />, label: "Home", path: "/", roles: ["doctor", "admin", "patient"] },
 
-  { icon: <FaCalendarCheck />, label: "Consultas", path: "/ScheduledAppointments", roles: ["doctor", "patient"] },
-  { icon: <FaSyringe />, label: "Exames", path: "/Exams", roles: ["doctor", "patient"] },
+  { icon: <FaCalendarCheck />, label: "Consultas", path: "/appointments", roles: ["doctor", "patient"] },
+  { icon: <FaSyringe />, label: "Exames", path: "/exams", roles: ["doctor", "patient"] },
 
-  { icon: <FaUserDoctor />, label: "Agendar Consulta", path: "/ScheduleAppointment", roles: ["patient"] },
+  { icon: <FaUserDoctor />, label: "Medicos", path: "/doctors", roles: ["patient"] },
 
   { icon: <MdManageAccounts />, label: "Gerenciar Médico", path: "/ManageDoctors", roles: ["admin"] },
   { icon: <IoFingerPrintSharp />, label: "Gerenciar Acesso", path: "/ManageAccess", roles: ["admin"] },
