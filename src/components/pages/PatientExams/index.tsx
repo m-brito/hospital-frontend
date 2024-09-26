0685a590cd88cba1cb91a453412ee8bc351f86ff
PolyStefane <polyvstefane@gmail.com>
2024-09-26 02:48:14 -0300

    fix: new patient exams route

---
+++ src/components/pages/PatientExams/index.tsx
@@ -0,0 +1,22 @@
+// External Libraries
+import React, { useEffect, useRef, useState } from "react";
+
+// Styles
+import {
+  Title,
+  ContainerCards,
+  
+} from "./styles";
+
+
+export const PatientExams: React.FC = () => {
+return (
+  <ContainerCards>
+
+    <Title>Lista de exames</Title>
+    
+  </ContainerCards>
+);
+};
+
+export default PatientExams;
---
+++ src/components/pages/PatientExams/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+export const ContainerCards = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  padding: 40px;
+  box-sizing: border-box;
+`;
+
+export const Title = styled.h1``;
+
